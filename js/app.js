8f8a02bdbc598073167abe39c64e602639e2b244
boots525 <alexisalawson@gmail.com>
2016-05-03 16:09:54 -0400

    second commit

--- js/app.js
+++ js/app.js
@@ -8,9 +8,17 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show(); 
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500, 
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+			); 
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
@@ -19,5 +27,9 @@ $(document).ready(function(){
 //animate hadouken to right of screen
 });
 
-
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
 
